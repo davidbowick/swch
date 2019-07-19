796cc8f2da2ce10fd250052e3c9f866e4e4d73df
David Bowick <dbowick@gmail.com>
2019-07-18 17:07:39 -0700

    eod

--- resources/views/admin/faqs/index.blade.php
+++ resources/views/admin/faqs/index.blade.php
@@ -4,15 +4,22 @@
 
 @if ($faqs)
 <div class="faqs container--thin">
-<h1 class="title">FAQs</a></h1>
+<h1 class="title">
+	FAQs 
+	@if(Auth::user())
+	@if(Auth::user()->type == 'admin')
+	<a href="/admin/faqs/create" class="btn" style="float: right;">Add New</a>
+	@endif 
+	@endif
+</h1>
 @foreach ($faqs as $faq)
 <div class="faq__item">
 	<h3 class="faq__question">
 		{{ $faq->question }}
 		@if(Auth::user())
-		@if(Auth::user()->type == 'admin')
-		<a href="/admin/faqs/{{$faq->id}}/edit" class="btn" style="float: right;">Edit</a>
-		@endif 
+			@if(Auth::user()->type == 'admin')
+			<a href="/admin/faqs/{{$faq->id}}/edit" class="btn" style="float: right;">Edit</a>
+			@endif 
 		@endif
 	</h3>
 	<div class="faq__answer rte"><p>{!! nl2br(e($faq->answer)) !!}</p></div>
