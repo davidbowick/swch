2a8e619b4b986cdf83202a0b6ee5e97c08d9903c
David Bowick <dbowick@gmail.com>
2019-08-15 15:35:57 -0700

    bunch of new stuff

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -4,7 +4,6 @@
 <hr class="hr--invisible">
 <div class="container--tiny">
     <h1 class="title">{{ __('Verify Your Email Address') }}</h1>
-
     @if (session('resent'))
     <div class="alert alert-success" role="alert">
         {{ __('A fresh verification link has been sent to your email address.') }}
@@ -12,7 +11,7 @@
     @endif
     <div class="rte">
     {{ __('Before proceeding, please check your email for a verification link.') }}
-    {{ __('If you did not receive the email') }}, <a href="{{ route('verification.resend') }}">{{ __('click here to request another') }}</a>.
+    {{ __('If you did not receive the email') }}, <a class="no-link" href="{{ route('verification.resend') }}">{{ __('click here to request another') }}</a>.
     </div>
 
 </div>
