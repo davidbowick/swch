ca67f26993e598be9fce4b10104ec4753cd2961b
David Bowick <dbowick@gmail.com>
2019-08-23 11:58:59 -0700

    Friday, August 23, 2019 at 11:58:47 AM

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -32,10 +32,11 @@
 
         <div class="field row">
             <div class="form-check">
+                <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
                 <label class="form-check-label" for="remember">
-                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
                     {{ __('Remember Me') }}
                 </label>
+
             </div>
         </div>
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -47,6 +47,15 @@
 					</span>
 					@enderror
 				</div>
+				<div class="field row">
+					<div class="form-check">
+						<input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+						<label class="form-check-label" for="remember">
+							{{ __('Remember Me') }}
+						</label>
+
+					</div>
+				</div>
 
 				<div class="field">
 					<button type="submit" class="btn btn--primary btn--full">
